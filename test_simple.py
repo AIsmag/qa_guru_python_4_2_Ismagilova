29a5a494c455ee62d0162d12bc84b5e4ec5a933f
Альбина <albina71994@yandex.ru>
2023-02-24 15:16:06 +0300

    Добавлен второй позитивный тест (с запросом на русском) и один негативный

--- test_simple.py
+++ test_simple.py
@@ -3,14 +3,18 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-@pytest.fixture
-def open_browser():
-    browser.open('https://google.com')
-
-
-def test_positive_in_english(open_browser):
+def test_positive_in_english(open_google):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_positive_in_russian(open_google):
+    browser.element('[name="q"]').should(be.blank).type('википедия').press_enter()
+    browser.element('[id="search"]').should(have.text('русская википедия'))
+
+
+def test_negative_in_english(open_google):
+    browser.element('[name="q"]').should(be.blank).type('gfhhdfhfhfhfghhfhf').press_enter()
+    browser.element('[id="search"]').should(have.text('ничего не найдено'))
 
 
 
