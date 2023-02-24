dd575df8f38b53d6329cde909b9276320f0deeed
Альбина <albina71994@yandex.ru>
2023-02-24 20:25:13 +0300

    Исправлен ОР в test_positive_in_russian

--- test_simple.py
+++ test_simple.py
@@ -1,4 +1,3 @@
-import pytest
 from selene.support.shared import browser
 from selene import be, have
 
@@ -7,16 +6,12 @@ def test_positive_in_english(open_google):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
+
 def test_positive_in_russian(open_google):
     browser.element('[name="q"]').should(be.blank).type('википедия').press_enter()
-    browser.element('[id="search"]').should(have.text('русская википедия'))
+    browser.element('[id="search"]').should(have.text('свободная энциклопедия'))
 
 
 def test_negative_in_english(open_google):
     browser.element('[name="q"]').should(be.blank).type('gfhhdfhfhfhfghhfhf').press_enter()
     browser.element('[id="search"]').should(have.text('ничего не найдено'))
-
-
-
-
-
