d56d81f7030e11dfc35acabd18c655327df5abba
Альбина <albina71994@yandex.ru>
2023-02-24 15:13:54 +0300

    Добавлен файл conftest.py. Старый был с неверным расширением

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selene.support.shared import browser
+
+@pytest.fixture
+def size_of_browser():
+    browser.config.window_width = 1900
+    browser.config.window_height = 1800
+
+
+@pytest.fixture
+def open_google(size_of_browser):
+    browser.open('https://google.com')
